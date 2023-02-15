8820412f295e308d2f1f7312dfb41ce493daef03
FelixB02 <felix.blechl@chello.at>
2023-02-15 20:03:29 +0100

    Files

---
+++ Basic.js
@@ -0,0 +1,42 @@
+"use strict";
+let done = true;
+let decimal = 15;
+let color = "Hello World";
+// Task1
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${j} * ${i} = ` + i * j);
+    }
+    console.log(`\n`);
+}
+// Task2
+const myName = [{ firstname: "Felix", lastname: "Blechl" }];
+const result2 = document.getElementById("content2");
+console.log(myName);
+for (let i = 0; i < 10; i++) {
+    myName.forEach(element => {
+        result2.innerHTML += `${element.firstname} <br>`;
+    });
+}
+setTimeout(function () {
+    for (let i = 0; i < 10; i++) {
+        myName.forEach(element => {
+            result2.innerHTML += `${element.lastname} <br>`;
+        });
+    }
+}, 5000);
+// Task3
+let arrayOfNames = ["Felix", "Paul", "Leo", "Jan", "Lara", "Johanna", "Laura"];
+const result1 = document.getElementById("content1");
+arrayOfNames.forEach(function (value, index) {
+    console.log(`${index}`);
+    result1.innerHTML += `${index} <br>`;
+});
+arrayOfNames.forEach(function (value, index) {
+    console.log(`${value}`);
+    result1.innerHTML += `${value} <br>`;
+});
+arrayOfNames.forEach(function (value, index) {
+    console.log(`${index}: ${value}`);
+    result1.innerHTML += `${index}: ${value} <br>`;
+});
